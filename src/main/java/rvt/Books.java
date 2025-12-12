654ed1c753e47813f6250f549d266201e8f148d2
24DP4TSibi <A240354TS@rvt.lv>
2025-12-12 09:48:23 +0200

    Add files via upload
    fixed

---
+++ src/main/java/rvt/Books.java
@@ -0,0 +1,15 @@
+
+
+public class Books {
+    private String title;
+    private int pages;
+    private int year;
+
+    public void Book(String title, int pages, int year ){
+        this.title = title;
+        this.pages = pages;
+        this.year = year;
+
+        
+    }
+}
